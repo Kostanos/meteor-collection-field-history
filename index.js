bba9633ff7f5280552acb861c8cca41ca266ddcf
Alberto Espinal <alberto.espinal.c@gmail.com>
2016-10-25 18:32:02 -0400

    Adding getHistoryOfFieldMethod

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ var CollectionFieldHistory = {
 };
 
 
-function attachCollectionFieldHistory(collection, opts){
+function attachCollectionFieldHistory(collection, opts) {
   if (!opts) opts = {};
   _.defaults(opts, CollectionFieldHistory);
   opts.fields.push(opts.lastModifiedField);
-  collection.before.insert(function(userId, doc){
+  collection.before.insert(function (userId, doc) {
     var history = {};
     doc[opts.lastModifiedField] = new Date();
     history = _.pick(doc, opts.fields);
-    if (!_.isEmpty(_.omit(_.clone(history), [opts.lastModifiedField]))){
+    if (!_.isEmpty(_.omit(_.clone(history), [opts.lastModifiedField]))) {
       doc[opts.field] = [history];
 
       opts.debug && console.log('before.insert FieldHistory', history);
     }
   });
-  collection.before.update(function(userId, doc, fieldNames, modifier, options){
+  collection.before.update(function (userId, doc, fieldNames, modifier, options) {
     var history = {};
-    if (modifier.$set){
+    if (modifier.$set) {
       modifier.$set[opts.lastModifiedField] = new Date();
       history = _.pick(modifier.$set, opts.fields);
     }
-    if (!_.isEmpty(_.omit(_.clone(history), [opts.lastModifiedField]))){
+    if (!_.isEmpty(_.omit(_.clone(history), [opts.lastModifiedField]))) {
       if (!modifier.$push) modifier.$push = {};
       modifier.$push[opts.field] = {$each: [history], $position: 0};
       opts.debug && console.log('before.update FieldHistory', doc, modifier, history);
@@ -38,8 +38,13 @@ function attachCollectionFieldHistory(collection, opts){
   });
 }
 
-Meteor.Collection.prototype.attachCollectionFieldHistory = function(opts){
-  attachCollectionFieldHistory(this,opts)
+Meteor.Collection.prototype.attachCollectionFieldHistory = function (opts) {
+  attachCollectionFieldHistory(this, opts)
 };
 
+function getHistoryOfField(object, field, historyField = CollectionFieldHistory.field) {
+  let history = object[historyField];
+  return history.filter(item => item[field]);
+}
 exports.attachCollectionFieldHistory = attachCollectionFieldHistory;
+exports.getHistoryOfField = getHistoryOfField;
